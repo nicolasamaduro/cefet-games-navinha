c0d1f67fd563fdc090530bba470bb9af61c1970e
Nicolas Maduro <nicolasamaduro@gmail.com>
2016-09-13 11:00:34 -0300

    pronto

--- core/src/br/cefetmg/games/util/Collision.java
+++ core/src/br/cefetmg/games/util/Collision.java
@@ -18,7 +18,7 @@ public class Collision {
      * @return true se há colisão ou false, do contrário.
      */
     public static final boolean circlesOverlap(Circle c1, Circle c2) {
-        return false;
+       return Math.pow(c1.radius+c2.radius,2)>Math.pow(c1.x-c2.x, 2)+Math.pow(c1.y-c2.y, 2);
     }
 
     /**
@@ -32,7 +32,14 @@ public class Collision {
     private static boolean rangesIntersect(
             float min1, float max1,
             float min2, float max2) {
+        if(max1>min2&&min1<min2)
+            return true;
+        if(min1<max2&&max1>max2)
+            return true;
+        if(min1>min2&&max1<max2)
+            return true;
         return false;
+        
     }
 
     /**
@@ -44,7 +51,6 @@ public class Collision {
      * @return true se há colisão ou false, do contrário.
      */
     public static final boolean rectsOverlap(Rectangle r1, Rectangle r2) {
-        return false;
+        return rangesIntersect(r1.x, r1.x+r1.width, r2.x, r2.x+r2.width)&&rangesIntersect(r1.y, r1.y+r1.height, r2.y, r2.y+r2.height); 
     }
-
 }
